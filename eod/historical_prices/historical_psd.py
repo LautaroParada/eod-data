92deacd8d90e84703178436d25e5cfe228e14b76
LautaroParada <lautaro.parada.opazo@gmail.com>
2021-05-06 12:25:48 -0400

    Live (Delayed) Stock Prices API

--- eod/historical_prices/historical_psd.py
+++ eod/historical_prices/historical_psd.py
@@ -7,10 +7,12 @@ Created on Wed May  5 08:39:48 2021
 
 from eod.historical_prices.stock_price_data_api import StockPriceData
 from eod.historical_prices.hist_splits_divs_shorts_api import SplitsDividendsShort
+from eod.historical_prices.live_delayed_api import LiveStockPrices
 
-class HistoricalPrices(StockPriceData, SplitsDividendsShort):
+class HistoricalPrices(StockPriceData, SplitsDividendsShort, LiveStockPrices):
     
     def __init__(self, api_key:str, timeout:int):
         # inhereting the API classes
         StockPriceData.__init__(self, api_key, timeout)
         SplitsDividendsShort.__init__(self, api_key, timeout)
+        LiveStockPrices.__init__(self, api_key, timeout)
---
+++ eod/historical_prices/live_delayed_api/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 12:12:29 2021
+
+@author: lauta
+"""
+
+from .live_delayed_stock_prices import LiveStockPrices
