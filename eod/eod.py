eb414c5c719f4039f707d37363a74041fdc49681
LautaroParada <lautaro.parada.opazo@gmail.com>
2021-05-10 10:06:33 -0400

    Initial structure of the SDK - Fundamental data

--- eod/eod.py
+++ eod/eod.py
@@ -6,8 +6,10 @@ Created on Wed May  5 09:06:37 2021
 """
 
 from eod.historical_prices import HistoricalPrices
+from eod.fundamental_economic_data import FundamentalEconomicData
 
-class EodHistoricalData(HistoricalPrices):
+class EodHistoricalData(HistoricalPrices, FundamentalEconomicData):
     def __init__(self, api_key:str, timeout:int=300):
         # Substructures of the API
         HistoricalPrices.__init__(self, api_key, timeout)
+        FundamentalEconomicData.__init__(self, api_key, timeout)
---
+++ eod/fundamental_economic_data/fundamental_economic.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 10:00:38 2021
+
+@author: lauta
+"""
+
+class FundamentalEconomicData():
+    def __init__(self, api:str, timeout:int):
+        pass
