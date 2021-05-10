1977a6630d08647c1d092bc0dc9ccae652f3e6bb
LautaroParada <lautaro.parada.opazo@gmail.com>
2021-05-10 15:23:15 -0400

    fundamental data
    implementation of the bond fundamental data. Additionally, some considerations and questions are proposed for the next steps of the sdk.

--- eod/fundamental_economic_data/fundamental_api/fundamental_data.py
+++ eod/fundamental_economic_data/fundamental_api/fundamental_data.py
@@ -12,9 +12,10 @@ class StockEtfFundsIndexFundamentalData(RequestHandler):
         # base URL's of the API
         self.URL_FUNDAMENTAL = 'https://eodhistoricaldata.com/api/fundamentals/'
         self.URL_BULK_FUNDAMENTALS = 'http://eodhistoricaldata.com/api/bulk-fundamentals/'
+        self.URL_BONDS_FUNDAMENTALS = 'https://eodhistoricaldata.com/api/bond-fundamentals/'
         super().__init__(api_key, timeout)
         
-    def get_fundamentals(self, symbol:str, **query_params):
+    def get_fundamentals_stock(self, symbol:str, **query_params):
         """
         Get fundamental data for the requested stock, etf, fund or index.
 
@@ -34,7 +35,7 @@ class StockEtfFundsIndexFundamentalData(RequestHandler):
         self.endpoint = self.URL_FUNDAMENTAL + symbol.upper()
         return super().handle_request(self.endpoint, query_params)
     
-    def get_bulk_fundamentals(self, symbol:str, **query_params):
+    def get_fundamentals_bulk(self, symbol:str, **query_params):
         """
         With this endpoint you able to download fundamental data for thousands 
         of companies in one request. It supports only stocks and doesn’t 
@@ -61,4 +62,26 @@ class StockEtfFundsIndexFundamentalData(RequestHandler):
 
         """
         self.endpoint = self.URL_BULK_FUNDAMENTALS + symbol.upper()
+        return super().handle_request(self.endpoint, query_params)
+    
+    def get_fundamentals_bonds(self, cusip:str, **query_params):
+        """
+        Get US corporate bonds fundamental data feed. Bonds fundamentals data 
+        could be accessed either via ISIN or via CUSIP IDs. Other IDs are not
+        supported at the moment.
+
+        Parameters
+        ----------
+        cusip : str
+            id of the corporate bond.
+        **query_params :
+            query parameters.
+
+        Returns
+        -------
+        dict
+            fundamental data for the corporate bond.
+
+        """
+        self.endpoint = self.URL_BONDS_FUNDAMENTALS + cusip.upper()
         return super().handle_request(self.endpoint, query_params)
--- sandbox.py
+++ sandbox.py
@@ -14,11 +14,15 @@ from eod import EodHistoricalData
 
 client = EodHistoricalData(api_key)
 symbol='AAPL.US'
+goverment_bond = 'SW10Y.GBOND'
+corporate_bond = 'US00213MAS35.BOND'
 
 #%% Historical Prices, Splits and Dividends Data API testing 
 
-resp = client.get_stock_prices(symbol, period='d', from_='1995-03-03', to='2021-04-24')
-resp = client.get_live_prices(symbol, s='GLD,QVAL')
+resp = client.get_stock_prices(symbol, period='d')
+resp = client.get_stock_prices(goverment_bond, period='d')
+resp = client.get_stock_prices(corporate_bond, period='d')
+resp = client.get_live_prices(corporate_bond, s='GLD,QVAL')
 resp = client.get_dividends(symbol, from_='2020-03-01', to='2021-04-24')
 resp = client.get_splits(symbol, from_='1994-01-01', to='2021-04-24')
 resp = client.get_short_interest(symbol, to='2021-04-24')
@@ -26,12 +30,31 @@ resp = client.get_intraday_data(symbol, interval='1m', from_='1620136800', to='1
 resp = client.get_stock_options(symbol)
 resp = client.get_stock_ta(symbol, function='sma', from_='2020-03-01', to='2021-05-07', period=10)
 
+# Questions and changes
+
+"""
+1. Change the get_stock_prices to get_prices_eod -> all the supported assets can be requested with this endpoint.
+2. Change get_stock_prices, get_live_prices and get_intraday_data to get_prices_something -> its the standard.
+3. Does the option endpoint support options for bonds and other instruments?
+4. Consider to include the full list of technical indicators as method.
+5. Consider to include the filter fields -> stock prices eod and Technical indicators
+
+"""
+
 #%% Fundamental and economic financial data
 
-resp = client.get_fundamentals('GSPC.INDX')
-# resp = client.get_bulk_fundamentals('AMEX')
+resp = client.get_fundamentals_stock(symbol)
+resp = client.get_fundamentals_bonds(cusip='US00213MAS35')
 resp = client.get_calendar_earnings(symbols='AAPL.US,MSFT.US,AI.PA', to='2021-12-31')
 resp = client.get_calendar_trends(symbols='AAPL.US,MSFT.US,AI.PA')
 resp = client.get_calendar_ipos()
 resp = client.get_calendar_splits()
 resp = client.get_macro_indicator('CHL')
+
+
+# Questions and changes
+
+"""
+1. Consider to include a method to list all available macro indicators
+
+"""
