8cfb274f2f2230af52eabc9b1bdfffa13603db04
LautaroParada <lautaro.parada.opazo@gmail.com>
2021-08-07 15:05:34 -0400

    Update sandbox.py
    coments on the testing - NOT FINISHED

--- sandbox.py
+++ sandbox.py
@@ -20,16 +20,22 @@ corporate_bond = 'US00213MAS35.BOND'
 
 #%% Historical Prices, Splits and Dividends Data API testing 
 
-resp = client.get_stock_prices('AAL.LSE', period='d')
+# Stock Price Data API (End-Of-Day Historical Data)
+resp = client.get_stock_prices('AAL.LSE', period='m', order='a') # stock prices - check
 resp = client.get_stock_prices(goverment_bond, period='d')
 resp = client.get_stock_prices(corporate_bond, period='d')
-resp = client.get_live_prices(corporate_bond, s='GLD,QVAL,QMOM')
-resp = client.get_dividends(symbol, from_='2020-03-01', to='2021-04-24')
-resp = client.get_splits(symbol, from_='1994-01-01', to='2021-04-24') # ERROR
-resp = client.get_short_interest(symbol, to='2021-06-30')
-resp = client.get_intraday_data(symbol, interval='1m', from_='1620136800', to='1620414000')
-resp = client.get_stock_options(symbol)
-resp = client.get_stock_ta(symbol, function='sma', from_='2020-03-01', to='2021-06-30', period=50)
+# Live (Delayed) Stock Prices API
+resp = client.get_live_prices(corporate_bond, s='GLD,QVAL,QMOM,EUR.FOREX,VTI,SW10Y.GBOND,US00213MAS35.BOND') # live prices - check
+# Historical Splits, Dividends and Short Interest API
+resp = client.get_dividends(symbol, from_='2000-03-01', to='2021-07-06') # dividends - check
+resp = client.get_splits(symbol, from_='1994-01-01', to='2021-07-04') # ERROR
+resp = client.get_short_interest(symbol, to='2021-07-04')
+# Intraday Historical Data API
+resp = client.get_intraday_data('EUR.FOREX', interval='5m', from_='1620136800', to='1620414000') # intraday data - check (not for bonds)
+# Options Data API
+resp = client.get_stock_options('AAPL') # Only for stocks - check
+# Technical Indicator API
+resp = client.get_stock_ta('EUR.FOREX', function='sma', from_='2020-03-01', to='2021-06-30', period=50, filter_='last_ema')
 
 
 # Questions and changes
@@ -37,9 +43,11 @@ resp = client.get_stock_ta(symbol, function='sma', from_='2020-03-01', to='2021-
 """
 1. Change the get_stock_prices to get_prices_eod -> all the supported assets can be requested with this endpoint.
 2. Change get_stock_prices, get_live_prices and get_intraday_data to get_prices_something -> its the standard.
-3. Does the option endpoint support options for bonds and other instruments?
-4. Consider to include the full list of technical indicators as method.
-5. Consider to include the filter fields -> stock prices eod and Technical indicators
+3. Does the option endpoint support options for bonds and other instruments? -> NO
+4. Check the from and to parameters of the get_stock_options endpoint
+5. Consider to include the full list of technical indicators as method.
+6. Consider to include the filter fields -> stock prices eod and Technical indicators
+7. Change the name of the get_stock_ta to get_instrument_ta, all instruments have ta available.
 
 """
 
