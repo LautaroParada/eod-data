bf8c50ee31dbcda272abc4ee531d3b328b8c73c9
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-06-15 14:47:35 -0400

    Update hist_splits_divs_shorts.py
    deletion of the short interest method

--- eod/historical_prices/splits_divs_shorts_api/hist_splits_divs_shorts.py
+++ eod/historical_prices/splits_divs_shorts_api/hist_splits_divs_shorts.py
@@ -12,7 +12,6 @@ class SplitsDividendsShort(RequestHandler):
         # base URL's of the API
         self.URL_DIVIDENDS = 'https://eodhistoricaldata.com/api/div/'
         self.URL_SPLITS = 'https://eodhistoricaldata.com/api/splits/'
-        self.URL_SHORT_INTEREST = 'https://eodhistoricaldata.com/api/shorts/'
         super().__init__(api_key, timeout)
         
     def get_dividends(self, symbol:str, **query_params):
@@ -53,25 +52,4 @@ class SplitsDividendsShort(RequestHandler):
 
         """
         self.endpoint = self.URL_SPLITS + symbol.upper()
-        return super().handle_request(self.endpoint, query_params)
-    
-    def get_short_interest(self, symbol:str, **query_params):
-        """
-        Historical short interest data for major stocks for the Nasdaq exchange.
-        Non-US exchanges are not supported at the moment.
-
-        Parameters
-        ----------
-        symbol : str
-            name of the stock to analyse, consists of two parts: {SYMBOL_NAME}.{EXCHANGE_ID}..
-        **query_params : TYPE
-            query parameters.
-
-        Returns
-        -------
-        list
-            historical short interest for the selected company.
-
-        """
-        self.endpoint = self.URL_SHORT_INTEREST + symbol.upper()
         return super().handle_request(self.endpoint, query_params)
