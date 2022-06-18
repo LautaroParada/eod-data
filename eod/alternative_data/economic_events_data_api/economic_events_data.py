a5f1f066d9c1e4f6b61a9581ac023d24410cabb4
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-06-17 21:50:10 -0400

    Economic events move
    I have moved the economic events API from the fundamental section to the alternative data section. This is because the structure of the API has changed.

--- eod/alternative_data/economic_events_data_api/economic_events_data.py
+++ eod/alternative_data/economic_events_data_api/economic_events_data.py
@@ -14,5 +14,21 @@ class EconomicEventsData(RequestHandler):
         super().__init__(api_key, timeout)
         
     def get_economic_events(self, **query_params):
+        """
+        provides the past and future events from all around the world like 
+        Retail Sails, Bond Auctions, PMI Releases and many other 
+        Economic Events data.
+
+        Parameters
+        ----------
+        **query_params :
+            
+
+        Returns
+        -------
+        dict
+            Past or future ecomic events for a country.
+
+        """
         self.endpoint = self.URL_ECONOMIC_EVENT_DATA
         return super().handle_request(self.endpoint, query_params)
--- eod/fundamental_economic_data/fundamental_economic.py
+++ eod/fundamental_economic_data/fundamental_economic.py
@@ -8,15 +8,13 @@ Created on Mon May 10 10:00:38 2021
 from eod.fundamental_economic_data.fundamental_api import StockEtfFundsIndexFundamentalData
 from eod.fundamental_economic_data.calendar_earnings_trends_ipos_splits_api import CalendarEarningsTrendsIposSplits
 from eod.fundamental_economic_data.insider_transactions_api import InsiderTransactions
-from eod.fundamental_economic_data.economic_events_data_api import EconomicEventsData
 from eod.fundamental_economic_data.historical_market_capitalization_api import HistoricalMarketCapitalization
 
 class FundamentalEconomicData(StockEtfFundsIndexFundamentalData, CalendarEarningsTrendsIposSplits,
-                              InsiderTransactions, EconomicEventsData, HistoricalMarketCapitalization):
+                              InsiderTransactions, HistoricalMarketCapitalization):
     def __init__(self, api_key:str, timeout:int):
         # inhereting the API classes
         StockEtfFundsIndexFundamentalData.__init__(self, api_key, timeout)
         CalendarEarningsTrendsIposSplits.__init__(self, api_key, timeout)
         InsiderTransactions.__init__(self, api_key, timeout)
-        EconomicEventsData.__init__(self, api_key, timeout)
         HistoricalMarketCapitalization.__init__(self, api_key, timeout)
