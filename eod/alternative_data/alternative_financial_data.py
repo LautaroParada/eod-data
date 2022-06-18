a24e6dcbe6f63197190b51fb9e749910c5e33f23
LautaroParada <lautaro.parada.opazo@gmail.com>
2022-06-17 21:39:25 -0400

    Macro indicators change
    I have reorganized the macroeconomic indicators API. Switched from the fundamental data to the alternative data.

--- eod/alternative_data/alternative_financial_data.py
+++ eod/alternative_data/alternative_financial_data.py
@@ -1,7 +1,10 @@
 from eod.alternative_data.sentiment_financial_api import SentimentFinancialData
 from eod.alternative_data.financial_news_api import FinancialNews
+from eod.alternative_data.macroeconomic_api import MacroEconomicIndicators
 
-class AlternativeFinancialDataEOD(SentimentFinancialData, FinancialNews):
+class AlternativeFinancialDataEOD(SentimentFinancialData, FinancialNews,
+                                  MacroEconomicIndicators):
     def __init__(self, api_key:str, timeout:int):
         SentimentFinancialData.__init__(self, api_key, timeout)
         FinancialNews.__init__(self, api_key, timeout)
+        MacroEconomicIndicators.__init__(self, api_key, timeout)
